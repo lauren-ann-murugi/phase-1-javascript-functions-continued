903874b7dc2ce2162345b87a5915a768a1e80654
laurenannmurugi <laurenann1871@gmail.com>
2025-06-04 21:36:33 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+// implementing a function called saturdayFun
+//default activity 'roller-skate' when no arguments are passed
+function saturdayFun(activity=`roller-skate`) {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+// implementing a function called mondayWork
+//default activity 'go to the office' when no arguments are passed
+function mondayWork(activity=`go to the office`) {
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+//implementing a function called wrapAdjective
+function wrapAdjective() {
+    const innerFunction = function (adjective = "special")  {
+        return `You are ${adjective}!`;
+    };
+    
+  };
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "special") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
+
+wrapAdjective("%")("a dedicated programmer");
